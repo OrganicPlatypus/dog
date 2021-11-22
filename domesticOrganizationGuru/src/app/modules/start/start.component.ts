06565878dfba1fcaf6b697a4ea79c50abf04bdb8
PawPaw <adamowiczpawel@gmail.com>
2021-11-22 21:59:10 +0100

    Map new redux state slice

--- domesticOrganizationGuru/src/app/modules/start/start.component.ts
+++ domesticOrganizationGuru/src/app/modules/start/start.component.ts
@@ -1,3 +1,4 @@
+import { TodoItem } from './../to-do-list/models/to-do';
 import { CreateNotesPackDto } from './../../services/service-proxy/service-proxy';
 import { OrganizerApiService } from 'src/app/services/api/api.service';
 import { Component, OnInit } from '@angular/core';
@@ -7,9 +8,8 @@ import { ConfigurationApiService } from 'src/app/services/api/configuration-api.
 import { Store } from '@ngrx/store';
 
 import * as SettingsActions from '../../state/states/settings/settings.actions';
-import { getMinutesTillExpireSelector } from '../../state/states/settings/settings.selector';
+import * as NotesActions from '../../state/states/notes/notes.actions';
 import { NoteSettingsState } from 'src/app/state/states/settings/settings.inteface';
-
 
 @Component({
   selector: 'app-start',
@@ -50,8 +50,16 @@ export class StartComponent implements OnInit {
 
   public joinSession(){
     this.organizerApiService.joinTheNote(this.joinSessionByName.value).subscribe((notesPack) => {
-    if(notesPack){
-      this.router.navigate(['/to-do']);
+      if(notesPack){
+        let todoItems : TodoItem[] = []
+        notesPack.notes?.map((note) => {
+          let todoItem = new TodoItem(note.noteText)
+          todoItem.isComplete = note.isComplete;
+          todoItems.push(todoItem)
+        })
+        this.store.dispatch(NotesActions.setExistingNotesAction({ notes : todoItems}))
+        this.store.dispatch(SettingsActions.setNoteNameAction({ noteName : this.joinSessionByName.value}))
+        this.router.navigate(['/to-do']);
     }
   });
   }
--- domesticOrganizationGuru/src/app/state/states/notes/notes.actions.ts
+++ domesticOrganizationGuru/src/app/state/states/notes/notes.actions.ts
@@ -1,5 +1,5 @@
+import { TodoItem } from './../../../modules/to-do-list/models/to-do';
 import { createAction, props } from "@ngrx/store";
-import { TodoItem } from "src/app/modules/to-do-list/models/to-do";
 
 export const setExistingNotesActionName = "[Root state] Sets existing notes";
 export const setExistingNotesAction = createAction(setExistingNotesActionName, props<{ notes: TodoItem[] }>());
